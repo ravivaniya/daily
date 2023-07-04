b6099253b09ed6c3885e6e5d2b08799f5b2bea8d
ravivaniya <ravivaniya4911@gmail.com>
2023-07-04 18:14:11 +0530

    add/buuble sort for numbers

---
+++ sorting/bubble.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void swap(int &num1, int &num2) {
+  int temp = num2;
+  num2 = num1;
+  num1 = temp;
+}
+
+void bubble_sort(vector<int> &arr, int n) {
+  for (int i = n - 1; i >= 0; i--) {
+    for (int j = 0; j <= i - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        swap(arr[j], arr[j+1]);
+      }
+    }
+  }
+  cout << "After bubble sort: " << "\n";
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+int main() {
+  vector<int> arr = {9, 4, 7, 6, 3, 1, 5};
+  int n = arr.size();
+  cout << "Before sorting: " << "\n";
+  for (int i=0; i<n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+  bubble_sort(arr, n);
+  return 0;
+}
